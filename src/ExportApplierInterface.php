b0d99181441ec2da5f25a39d02b7483538b895bb
Lucian Sabo <luciansabo@gmail.com>
2023-04-12 16:02:49 +0300

    updated phpdoc in ExportApplierInterface

--- src/ExportApplierInterface.php
+++ src/ExportApplierInterface.php
@@ -6,7 +6,7 @@ interface ExportApplierInterface
 {
     /**
      * @param object|array $data
-     * @param array $fields
+     * @param array|null $fields
      * @return object|array $data with exported fields
      */
     public function setExportedFields(/*object|array*/ $data, ?array $fields);
